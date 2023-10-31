39b866c9ee318eb2f19fe9432e370c897cfe33b1
Rakutenipoi <43430994+Rakutenipoi@users.noreply.github.com>
2023-10-31 11:39:57 +0800

    添加数据保存
    以numpy格式保存数据

--- src/data process/process_dataset.py
+++ src/data process/process_dataset.py
@@ -1,6 +1,17 @@
 import json
 import os
-from read_dataset import read_folder
+from read_dataset import read_folder_files_json
 
 
 
+
+
+
+
+
+if __name__ == "__main__":
+    json_folder_path = "D:/Study/Projects/DeepLearning/Resources/Indoor/3D-FRONT/"
+
+    data = read_folder_files_json(json_folder_path, 10)
+
+    print("done")
---
+++ src/data process/write_dataset.py
@@ -0,0 +1,10 @@
+import numpy as np
+import os
+
+def write_data_in_numpy(data, path, file_name = "data.npy"):
+    file_path = os.path.join(path, file_name)
+    np.save(file_path, data)
+
+if __name__ == "__main__":
+    data = [{'name': 'yuanshen'}]
+    write_data_in_numpy(data, "D:/Study/Projects/DeepLearning/UrbanGeneration/Indoor/ATISS Remake/")
