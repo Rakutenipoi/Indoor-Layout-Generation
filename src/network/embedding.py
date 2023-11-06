0e5bc01f051bfa8c7cd692da2dbbe8c10378cfe9
Rakutenipoi <43430994+Rakutenipoi@users.noreply.github.com>
2023-11-06 20:36:54 +0800

    网络总体的完善
    1. 将embedding与positional encoding部分添加到网络中，总体的结构已大致确定，还需要加入对预测分布的采样
    2. embedding的细节还没确定，关于sequence的尺寸也没确定

--- src/network/embedding.py
+++ src/network/embedding.py
@@ -3,43 +3,84 @@ import torch.nn as nn
 import math
 
 class PositionEncoding(nn.Module):
-    def __init__(self, class_num, encode_levels=128, E_dims=256):
+    def __init__(self, class_num, sequence_length, encode_levels=128, E_dims=256):
         super(PositionEncoding, self).__init__(class_num, encode_levels)
-        self.E_class = nn.Parameter(torch.randn(class_num, E_dims)) # E_class的存在形式未定
+        self.E_class = nn.Parameter(torch.randn(class_num, E_dims))
         self.encode_levels = encode_levels
+        self.sequence_length = sequence_length
+        self.E_dims = E_dims
 
     def forward(self, x):
-        if isinstance(x, int):
-            return self.E_class[x, :]
-        else:
-            angles = torch.arange(128).float()  # 生成角度向量，范围为0到127
-            frequencies = 2 ** angles * math.pi  # 生成频率向量
-            sin_encodings = torch.sin(frequencies * x.unsqueeze(1))  # 计算正弦编码
-            cos_encodings = torch.cos(frequencies * x.unsqueeze(1))  # 计算余弦编码
-            encodings = torch.stack([sin_encodings, cos_encodings], dim=2)  # 按照 sin、cos 排布的顺序进行拼接
-            encodings = encodings.view(-1, 256)  # 将编码向量展平为一维向量
-            return encodings
+        sequence = torch.zeros(self.max_sequence_length, self.E_dims)
+        idx = 0
+
+        for token in x:
+            if isinstance(token, int):
+                sequence[idx] = self.E_class[x, :]
+            else:
+                angles = torch.arange(128).float()  # 生成角度向量，范围为0到127
+                frequencies = 2 ** angles * math.pi  # 生成频率向量
+                sin_encodings = torch.sin(frequencies * x.unsqueeze(1))  # 计算正弦编码
+                cos_encodings = torch.cos(frequencies * x.unsqueeze(1))  # 计算余弦编码
+                encoding = torch.stack([sin_encodings, cos_encodings], dim=2)  # 按照 sin、cos 排布的顺序进行拼接
+                encoding = encoding.view(-1, 256)  # 将编码向量展平为一维向量
+                sequence[idx] = encoding
+            idx += 1
+
+        return sequence
+
 class RelativePositionEncoding(nn.Module):
-    def __init__(self, attributes_num=8, E_dims=256):
+    def __init__(self, max_sequence_length, attributes_num=8, E_dims=256):
         super(RelativePositionEncoding, self).__init__()
         self.E_relative_position = nn.Parameter(torch.randn(attributes_num, E_dims))
+        self.attributes_num = attributes_num
+        self.E_dims = E_dims
+        self.max_sequence_length = max_sequence_length
 
     def forward(self, x):
-        return self.E_relative_position[x, :]
+        sequence = torch.zeros(self.max_sequence_length, self.E_dims)
+        idx = 0
+
+        for token in x:
+            sequence[idx] = self.E_relative_position[idx % self.attributes_num, :]
+            idx += 1
+
+        return sequence
 class ObjectIndexEncoding(nn.Module):
-    def __init__(self, object_max_num, E_dims=256):
+    def __init__(self, max_sequence_length, object_max_num, attributes_num=8, E_dims=256):
         super().__init__()
         self.E_object_index = nn.Parameter(torch.randn(object_max_num, E_dims))
+        self.E_dims = E_dims
+        self.max_sequence_length = max_sequence_length
+        self.attributes_num = attributes_num
 
     def forward(self, x):
-        return self.E_object_index[x, :]
+        sequence = torch.zeros(self.max_sequence_length, self.E_dims)
+        idx = 0
+
+        for token in x:
+            if idx % self.attributes_num == 0:
+                type = token
+            sequence[idx] = self.E_object_index[type, :]
+            idx += 1
+
+        return sequence
 
 class AbsolutePositionEncoding(nn.Module):
-    def __init__(self, object_num, E_dims=256):
+    def __init__(self, max_sequence_length, object_num, E_dims=256):
         super().__init__()
         self.E_absolute_position = nn.Parameter(torch.randn(object_num, E_dims))
+        self.E_dims = E_dims
+        self.max_sequence_length = max_sequence_length
 
     def forward(self, x):
-        return self.E_absolute_position[x, :]
+        sequence = torch.zeros(self.max_sequence_length, self.E_dims)
+        idx = 0
+
+        for token in x:
+            sequence[idx] = self.E_absolute_position[idx / 8, :]
+            idx += 1
+
+        return sequence
 
 
---
+++ src/network/network.py
@@ -0,0 +1,88 @@
+from embedding import PositionEncoding, RelativePositionEncoding, ObjectIndexEncoding, AbsolutePositionEncoding
+import torch
+import torch.nn as nn
+import math
+from transformer_modules import EncoderLayer, DecoderLayer
+from boundary_encoder import get_boundary_encoder
+
+class cofs_network(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.n_layers = config['network']['n_layers']
+        self.n_heads = config['network']['n_heads']
+        self.dimensions = config['network']['dimensions']
+        self.d_ff = config['network']['feed_forward_dimensions']
+        self.droupout = config['training']['droupout']
+        self.max_sequence_length = config['network']['max_sequence_length']
+        self.attributes_num = config['data']['attributes_num']
+        self.object_max_num = config['data']['object_max_num']
+        self.class_num = config['data']['class_num'] + 1 # 需要预留一个额外的类别用于标记起始与结束
+
+        # 用max_sequence_length替代sequence_length
+        self.embedding = PositionEncoding(
+            class_num=self.class_num,
+            sequence_length=self.max_sequence_length,
+            encode_levels=self.dimensions / 2,
+            E_dims=self.dimensions
+        )
+
+        self.relative_position_encoding = RelativePositionEncoding(
+            max_sequence_length=self.max_sequence_length,
+            attributes_num=self.attributes_num,
+            E_dims=self.dimensions
+        )
+        self.object_index_encoding = ObjectIndexEncoding(
+            max_sequence_length=self.max_sequence_length,
+            object_max_num=self.object_max_num,
+            attributes_num=self.attributes_num,
+            E_dims=self.dimensions
+        )
+        self.absolute_position_encoding = AbsolutePositionEncoding(
+            max_sequence_length=self.max_sequence_length,
+            object_num=self.object_max_num,
+            E_dims=self.dimensions
+        )
+
+        self.condition_encoder = nn.ModuleList(
+            [EncoderLayer(self.dimensions, self.n_heads, self.d_ff, self.dropout) for _ in range(self.n_layers)]
+        )
+        self.generative_decoder = nn.ModuleList(
+            [DecoderLayer(self.dimensions, self.n_heads, self.d_ff, self.dropout) for _ in range(self.n_layers)]
+        )
+        self.boundary_encoder = get_boundary_encoder(
+            config['boundary_encoder']['name'],
+            config['boundary_encoder']['freeze_bn'],
+            config['boundary_encoder']['input_channels'],
+            config['boundary_encoder']['feature_size']
+        )
+
+    def forward(self, sequence, layout_image, last_sequence):
+        # Boundary Encoder
+        layout_feature = self.boundary_encoder(layout_image)
+
+        # Positional Encoding
+        relative_position = self.relative_position_encoding(sequence)
+        object_index = self.object_index_encoding(sequence)
+        absolute_position = self.absolute_position_encoding(last_sequence)
+
+        # Embedding
+        sequence = self.embedding(sequence)
+        last_sequence = self.embedding(last_sequence)
+
+        # Input Blend & Concatenate
+        sequence = sequence + relative_position + object_index
+        sequence = torch.concat((layout_feature, sequence), dim=0)
+        last_sequence = last_sequence + absolute_position
+        last_sequence = torch.concat((torch.zeros((1, self.dimensions)), last_sequence), dim=0)
+
+        # Encoders Process
+        encoder_output = sequence
+        for encoder_layer in self.condition_encoder:
+            encoder_output = encoder_layer(encoder_output, None)
+
+        # Decoders Process
+        decoder_output = last_sequence
+        for decoder_layer in self.generative_decoder:
+            decoder_output = decoder_layer(decoder_output)
+
+        # Output Sample
