982b0d79d1e5f3f916cefa966a6e9d1272095673
Rakutenipoi <43430994+Rakutenipoi@users.noreply.github.com>
2024-05-17 18:09:54 +0800

    优化损失计算

--- src/utils/loss_cal.py
+++ src/utils/loss_cal.py
@@ -24,13 +24,15 @@ def gaussian_loss(predict, truth):
     v = torch.clamp(v, min=epsilon)
 
     # 计算logistic分布的概率密度函数值
-    gaussian_prob = torch.exp(-0.5 * torch.pow(truth.unsqueeze(-1) - mu, 2) / v) / (torch.sqrt(v * 2 * torch.pi))
+    # gaussian_prob = torch.exp(-0.5 * torch.pow(truth.unsqueeze(-1) - mu, 2) / v) / (torch.sqrt(v * 2 * torch.pi))
     # diff_x = (truth.unsqueeze(-1) - mu) / v * 0.5
     # logistic_prob = 1 / (v * torch.pow(torch.exp(diff_x) + torch.exp(-diff_x), 2))
+    normal_dist = dist.Normal(mu, torch.sqrt(v))
+    gaussian_prob = normal_dist.log_prob(truth.unsqueeze(-1)).exp()
 
     # 计算每个高斯分布的权重
     weight = predict[:, :, :, 0]  # 预测结果的权重
-    weight = F.softmax(weight, dim=-1)
+    # weight = F.softmax(weight, dim=-1)
 
     # 将预测结果按照最后一个维度求和，得到所有高斯分布的混合概率密度函数值
     prob = gaussian_prob * weight
@@ -44,8 +46,8 @@ def gaussian_loss(predict, truth):
 
     return loss
 
-def get_padding_mask(seq_length, batch_size=32):
-    padding_mask = torch.ones((batch_size, 105), device=device)
+def get_padding_mask(seq_length, batch_size=32, sequence_length=105):
+    padding_mask = torch.ones((batch_size, sequence_length), device=device)
     for i, length in enumerate(seq_length):
         padding_mask[i, length.item():] = 0.0
 
@@ -59,25 +61,32 @@ def loss_calculate(src, output, src_len, config):
     max_len = attributes_num * object_max_num
     sample_num = config['network']['sampler']['output_dimension'] // 3
     batch_size = src.size(0)
+
     # 提取分类结果
     output_class = output[:, ::attributes_num, :class_num]
-    # output_class = torch.argmax(output_class, dim=2)
     src_class = src[:, ::attributes_num]
+
     # 提取回归结果
     slices = [output[:, start:start + attributes_num - 1, :] for start in range(1, max_len, attributes_num)]
     output_attr = torch.cat(slices, dim=1)
     slices = [src[:, start:start + attributes_num - 1] for start in range(1, max_len, attributes_num)]
     src_attr = torch.cat(slices, dim=1)
+
     # 回归结果采样
     output_attr = output_attr.view(batch_size, -1, sample_num, 3)
 
     # 计算分类损失
-    loss_class = F.cross_entropy(output_class.view(-1, class_num), src_class.to(torch.int64).view(-1), ignore_index=0)
+    slices = [output_class[i, :src_len[i], :] for i in range(batch_size)]
+    output_class = torch.cat(slices, dim=0)
+    slices = [src_class[i, :src_len[i]] for i in range(batch_size)]
+    src_class = torch.cat(slices, dim=0)
+    loss_class = F.cross_entropy(output_class, src_class.to(torch.int64), reduction='sum') / batch_size
 
     # 计算回归损失
     mask = get_padding_mask(src_len * (attributes_num - 1), batch_size)
     loss_property = gaussian_loss(output_attr, src_attr)
     loss_property = loss_property * mask
+
     # 对损失进行平均
     loss_property = torch.sum(loss_property, dim=-1).mean()
 
